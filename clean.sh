1d3cad12ba2647ee93b11916c9206b48fbd35d28
vaudois <info@vaudois.ch>
2023-02-26 02:37:39 +0100

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,12 @@
+strip -s stratum
+sudo rm -f *.o
+sudo rm -f algos/*.o
+sudo rm -f algos/*.a
+sudo rm -f sha3/*.o
+sudo rm -f sha3/*.a
+sudo rm -f algos/ar2/*.o
+sudo rm -f algos/blake2/*.o
+sudo rm -f algos/blake2-ref/*.o
+sudo rm -f algos/honeycomb/*.o
+sudo rm -f algos/SWIFFTX/*.o
+sudo rm -f algos/yespower/*.o
